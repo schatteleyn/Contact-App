013ae7b92d2df8363f9aa1c6d0304838a9ea7cdf
Thomas Rohée <roheethomas@gmail.com>
2011-04-04 23:09:19 +0200

    sqliteSetup

--- CompanyAddressBook/CompanyAddressBook/CompanyAddressBookAppDelegate.h
+++ CompanyAddressBook/CompanyAddressBook/CompanyAddressBookAppDelegate.h
@@ -7,13 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @interface CompanyAddressBookAppDelegate : NSObject <UIApplicationDelegate> {
 
     UIWindow *_window;
 	UINavigationController *_navigationController;
+	
+	NSString *databaseName;
+	NSString *databasePath;
+	
+	NSMutableArray *contacts;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+@property (nonatomic, retain) NSMutableArray *contacts;
 @end
